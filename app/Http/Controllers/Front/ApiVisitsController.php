070d62879fab6fe5b38e64c06f775f820fb63f51
ayla <alaa.srh123@gmail.com>
2026-07-12 10:55:50 +0300

    push latest updates

--- app/Http/Controllers/Front/ApiVisitsController.php
+++ app/Http/Controllers/Front/ApiVisitsController.php
@@ -15,29 +15,6 @@ class ApiVisitsController extends Controller
     use ResponseTrait;
 
 
-    // public function pat_visits():JsonResponse{
-
-    //     // $user = auth()->user();
-    //     $userID = $user->id;
-    //     // $patient = gnr_m_patients::where('user_id',$userID)->first();
-    //   $patient = gnr_m_patients::find(1); 
-
-    // $visits = cln_x_visits::with('gnr_m_clinics')->where('patient', '=', $patient->id)->get();
-    //     if (!$patient){
-    //         return $this->returnError("D01","there are no visits");
-    //     }else{
-    //         $visits = cln_x_visits::with('gnr_m_clinics')->where('patient','=',$patient->id)->get();
-    //         if ($visits->count() == 0){
-    //             return $this->returnError("D01","there are no visits");
-    //         }else{
-    //             foreach ($visits as $visit){
-    //                 $newdate = Carbon::parse($visit->d_start)->format('Y-m-d الساعة: h:i A');
-    //                 $visit->d_start = $newdate;
-    //             }
-    //             return $this->returnData("visits",$visits,"hi");
-    //         }
-    //     }
-    // }
     public function pat_visits(): JsonResponse 
 {
     // 1. جلب المستخدم الذي سجل دخوله
@@ -49,7 +26,7 @@ class ApiVisitsController extends Controller
 
     // 2. البحث عن المريض المرتبط بهذا المستخدم
     $patient = gnr_m_patients::where('user_id', $user->id)->first();
-
+   
     if (!$patient) {
         return $this->returnError("D01", "بيانات المريض غير موجودة لهذا المستخدم");
     }
@@ -70,17 +47,5 @@ class ApiVisitsController extends Controller
     return $this->returnData("visits", $visits, "تم جلب البيانات بنجاح");
 }
 
-//    public function fucking_database(){
-//        $visits = cln_x_visits::all();
-//        if (!$visits){
-//            return"fuck off";
-//        }else{
-//            foreach ($visits as $visit){
-//                $newdate = Carbon::parse($visit->d_start)->format('Y-m-d الساعة: h:i A');
-//                $visit->d_start = $newdate;
-//                $visit->save();
-//            }
-//            return "DataBase Fucked Successfully...";
-//        }
-//    }
+
 }
